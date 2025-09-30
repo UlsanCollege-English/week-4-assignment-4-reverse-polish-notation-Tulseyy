fdd3abc06ea993d03431685ba2b0babfa5f21084
tulseyy <sashankartulsha@gmail.com>
2025-09-30 12:37:51 +0900

    polish

--- src/rpn.py
+++ src/rpn.py
@@ -1,5 +1,25 @@
 
 
 def eval_rpn(tokens):
-    # TODO: implement RPN evaluation using a stack
-    raise NotImplementedError
+    stack = []
+    operators = {"+", "-", "*", "/"}
+    
+    for token in tokens:
+        if token not in operators:
+            # token is a number, push it as int
+            stack.append(int(token))
+        else:
+            # token is an operator; pop two operands
+            b = stack.pop()
+            a = stack.pop()
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            else:  # token == "/"
+                # integer division that truncates toward zero
+                stack.append(int(a / b))
+    
+    return stack.pop()
